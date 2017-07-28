f473911b9d499c5f942824789465f57feb28d054
fengyong <373209197@qq.com>
2017-07-28 16:39:38 +0800

    change the computer to continue developing because my computer is downloading some videos

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,14 @@ export default {
 </script>
 
 <style lang="scss">
-
+  html,body{
+      width: 100%;
+      height: 100%;
+      font-size: 62.5%;
+  }
+#app{
+  width: 100%;
+  height: 100%;
+  background: url("./image/loginbackground.jpg");
+}
 </style>
